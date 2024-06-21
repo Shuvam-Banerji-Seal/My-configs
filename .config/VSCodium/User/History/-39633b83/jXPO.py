d4e1647351d65b8cf13f4c023dcaf8fb7089df54
Shuvam Banerji Seal <118100973+Shuvam-Banerji-Seal@users.noreply.github.com>
2024-06-21 20:06:20 +0530

    Delete .config directory

--- .config/VSCodium/User/History/-39633b83/jXPO.py
+++
@@ -1,5 +0,0 @@
-
-# Example usage
-input_file_path = '/home/shuvam/Information_Retrieval/TREC_ToT_baselines/Corpu_and_dataset/corpus.jsonl'
-output_file_path = '/home/shuvam/Information_Retrieval/Corpus_analysis/key.jsonl'
-extract_keywords_from_corpus(input_file_path, output_file_path)
